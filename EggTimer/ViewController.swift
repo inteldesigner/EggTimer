de3668bbb13141c9cc68876caf0615d9581c50f0
Eric <ericstein.rl@gmail.com>
2019-06-13 18:40:18 -0500

    Initial Commit

---
+++ EggTimer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EggTimer
+//
+//  Created by Eric Stein on 6/13/19.
+//  Copyright © 2019 Eric Stein. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
